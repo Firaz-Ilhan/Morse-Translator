6ae87e764a16463a48926f1a407ef89020fb8dba
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2020-08-20 13:50:16 +0200

    rearange morse to words tab

--- src/main/java/org/translator/GuiController.java
+++ src/main/java/org/translator/GuiController.java
@@ -3,6 +3,7 @@ package org.translator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,9 +12,9 @@ public class GuiController {
     private final static Logger log = LogManager.getLogger(GuiController.class);
 
     @FXML
-    private TextField morseInput;
+    private TextArea morseInput;
     @FXML
-    private Label wordsOutput;
+    private TextArea wordsOutput;
 
     @FXML
     public void morseToWords(ActionEvent actionEvent) {
